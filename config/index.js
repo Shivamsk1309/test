4267a93f5d763f0f2fbf07dc69313d483bed82cd
Robin Kumar <robin.kumar@edfora.com>
2024-12-01 18:37:21 +0530

    initial commit

---
+++ config/index.js
@@ -0,0 +1,18 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+const fs = require('fs');
+
+const {
+  NODE_ENV = 'development',
+} = process.env;
+
+const CONFIG_FILE_PATH = `${__dirname}/${NODE_ENV}.config.json`;
+
+function getConfig() {
+  const fileData = fs.readFileSync(CONFIG_FILE_PATH, { encoding: 'utf8', flag: 'r' });
+  const config = JSON.parse(fileData);
+  return config;
+}
+
+module.exports = {
+  getConfig,
+};
