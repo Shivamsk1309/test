5a4da49366b6c214e3f7673de1247889c3281ff2
Shivam Kaushik <skshivam.1309@gmail.com>
2024-12-03 22:13:51 +0530

    added branch

--- modules/loginSignUp/helper/loginSignUp.helper.ts
+++ modules/loginSignUp/helper/loginSignUp.helper.ts
@@ -1,13 +1,13 @@
-import bcrypt from "bcrypt";
-import twilio from "twilio";
-import { throwValidationError } from "../../../utils/commonUtils/commonUtils";
-import { createJwtToken } from "../../../utils/jwt/jwtUtils";
+import bcrypt from 'bcrypt';
+import twilio from 'twilio';
+import { throwValidationError } from '../../../utils/commonUtils/commonUtils';
+import { createJwtToken } from '../../../utils/jwt/jwtUtils';
 const accountSid = process.env.TWILIO_ACCOUNT_SID!;
 const authToken = process.env.TWILIO_AUTH_TOKEN!;
 
 // Ensure these are being loaded properly
 if (!accountSid || !authToken) {
-  throw new Error("Twilio account SID and auth token are required!");
+  throw new Error('Twilio account SID and auth token are required!');
 }
 
 const twilioClient = twilio(accountSid, authToken);
@@ -21,9 +21,9 @@ const sendVerificationCodeHelper = async (phoneNumber: any) => {
       from: process.env.TWILIO_PHONE_NUMBER!, // Ensure the Twilio phone number is set
       to: phoneNumber,
     });
-    console.log("Message sent successfully:", twilioData);
+    console.log('Message sent successfully:', twilioData);
   } catch (error) {
-    console.error("Error sending message:", error);
+    console.error('Error sending message:', error);
   }
 };
 
@@ -38,12 +38,12 @@ const checkLoginHelper = async ({
   email,
 }: checkLoginHelperParams) => {
   // get user with email or mobileNumber TODO
-  const user: { email: string; password: string } = { email: "", password: "" };
+  const user: { email: string; password: string } = { email: '', password: '' };
   const { password: hashPassword, email: userEmail } = user;
   const isValidPassword = bcrypt.compareSync(password, hashPassword);
   if (!isValidPassword) {
     throwValidationError({
-      message: "Invalid Password",
+      message: 'Invalid Password',
     });
   }
   const token = createJwtToken({ tokenParams: { email: userEmail } });
