1c20a1870232276d07500afadd8844d810afa5bd
Shivam Kaushik <skshivam.1309@gmail.com>
2024-12-03 22:09:08 +0530

    added branch

--- modules/loginSignUp/helper/loginSignUp.helper.ts
+++ modules/loginSignUp/helper/loginSignUp.helper.ts
@@ -1,7 +1,7 @@
-import bcrypt from 'bcrypt';
-import twilio from 'twilio';
-import { throwValidationError } from '../../../utils/commonUtils/commonUtils';
-import { createJwtToken } from '../../../utils/jwt/jwtUtils';
+import bcrypt from "bcrypt";
+import twilio from "twilio";
+import { throwValidationError } from "../../../utils/commonUtils/commonUtils";
+import { createJwtToken } from "../../../utils/jwt/jwtUtils";
 const accountSid = process.env.TWILIO_ACCOUNT_SID!;
 const authToken = process.env.TWILIO_AUTH_TOKEN!;
 
@@ -14,43 +14,41 @@ const twilioClient = twilio(accountSid, authToken);
 
 const sendVerificationCodeHelper = async (phoneNumber: any) => {
   console.log(phoneNumber);
-  const code = Math.floor(100000 + Math.random() * 900000).toString(); 
+  const code = Math.floor(100000 + Math.random() * 900000).toString();
   try {
     let twilioData = await twilioClient.messages.create({
       body: `Your verification code is ${code}`,
-      from: process.env.TWILIO_PHONE_NUMBER!,  // Ensure the Twilio phone number is set
+      from: process.env.TWILIO_PHONE_NUMBER!, // Ensure the Twilio phone number is set
       to: phoneNumber,
     });
-    console.log('Message sent successfully:', twilioData);
+    console.log("Message sent successfully:", twilioData);
   } catch (error) {
-    console.error('Error sending message:', error);
+    console.error("Error sending message:", error);
   }
-  
 };
 
 interface checkLoginHelperParams {
-  password: string,
-  mobileNumber?: string,
-  email?: string,
+  password: string;
+  mobileNumber?: string;
+  email?: string;
 }
 const checkLoginHelper = async ({
-  mobileNumber, password, email,
+  mobileNumber,
+  password,
+  email,
 }: checkLoginHelperParams) => {
   // get user with email or mobileNumber TODO
-  const user: { email: string, password: string } = { email: '', password: '' };
+  const user: { email: string; password: string } = { email: "", password: "" };
   const { password: hashPassword, email: userEmail } = user;
   const isValidPassword = bcrypt.compareSync(password, hashPassword);
   if (!isValidPassword) {
     throwValidationError({
-      message: 'Invalid Password',
+      message: "Invalid Password",
     });
   }
   const token = createJwtToken({ tokenParams: { email: userEmail } });
   // save token in db TODO
   return { authToken: token };
-}
+};
 
-export {
-  checkLoginHelper,
-  sendVerificationCodeHelper,
-}
+export { checkLoginHelper, sendVerificationCodeHelper };
